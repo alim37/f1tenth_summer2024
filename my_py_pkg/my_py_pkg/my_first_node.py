5c6d7a1d4a816fce56f5c846e668e125f3c3faf0
Malik Ali <158620884+alim37@users.noreply.github.com>
2024-05-15 10:10:42 -0400

    Update my_first_node.py

--- my_py_pkg/my_py_pkg/my_first_node.py
+++ my_py_pkg/my_py_pkg/my_first_node.py
@@ -2,12 +2,25 @@
 import rclpy
 from rclpy.node import Node
 
+# Object oriented programming, create class
+class MyNode(Node):
+
+    def __init__(self):
+        super().__init__("py_test")
+        self.counter_ = 0
+        self.get_logger().info("Hello ROS2")          
+        self.create_timer(0.5, self.timer_callback)
+
+    def timer_callback(self):
+        self.counter_ += 1
+        self.get_logger().info("Hello " + str(self.counter_))
+
+
 def main(args=None):
     rclpy.init(args=args)   # must be the first line written in any ROS2 program
 
-    node = Node("py_test")
-    node.get_logger().info("Hello ROS2")
-    rclpy.spin(node)        # continues running program after shutdown (important for future uses)
+    node = MyNode()
+    rclpy.spin(node)
 
     rclpy.shutdown()        # must be last line
 
