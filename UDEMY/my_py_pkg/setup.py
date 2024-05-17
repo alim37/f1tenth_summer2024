64af349c96c74493201ec0c26ff31d5acd978296
Malik Ali <158620884+alim37@users.noreply.github.com>
2024-05-17 09:39:36 -0400

    Update setup.py

--- UDEMY/my_py_pkg/setup.py
+++ UDEMY/my_py_pkg/setup.py
@@ -21,7 +21,9 @@ setup(
     entry_points={
         'console_scripts': [
             "py_node = my_py_pkg.my_first_node:main",
-            "robot_news_station = my_py_pkg.robot_news_station:main"
+            "robot_news_station = my_py_pkg.robot_news_station:main",
+            "number_publisher = my_py_pkg.number_publisher:main",
+            "number_counter = my_py_pkg.number_counter:main"
         ],
     },
 )
