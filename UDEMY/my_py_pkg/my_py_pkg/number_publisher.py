b911801cb00b721e5470ac70deb37e8b2cacf370
Malik Ali <158620884+alim37@users.noreply.github.com>
2024-05-20 11:45:40 -0400

    Update number_publisher.py

--- UDEMY/my_py_pkg/my_py_pkg/number_publisher.py
+++ UDEMY/my_py_pkg/my_py_pkg/number_publisher.py
@@ -7,8 +7,12 @@ from example_interfaces.msg import Int64
 class NumberPublisherNode(Node):      # MODIFY NAME
 
     def __init__(self):
-        super().__init__("number_publisher")  # MODIFY NAME
-        self.number_ = 2
+        super().__init__("number_publisher")  
+
+        # declare parameter to change at runtime, default parameter if you don't change is 2. 
+        self.declare_paramter("number_to_publish", 2)
+        self.number_ = self.get_parameter("number_to_publish").value
+        
         self.publisher_ = self.create_publisher(Int64, "number", 10)
         self.number_timer_ = self.create_timer(1.0, self.publish_number)
         self.get_logger().info("Number publisher has been started.")
