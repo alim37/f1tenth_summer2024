0795c8a0dc7b838999d2679663ac50011494ed29
Malik Ali <158620884+alim37@users.noreply.github.com>
2024-06-26 12:22:06 -0400

    Update pure_pursuit.py

--- f1tenth labs/lab1_ws/src/lab5_pkg/lab5_pkg/pure_pursuit.py
+++ f1tenth labs/lab1_ws/src/lab5_pkg/lab5_pkg/pure_pursuit.py
@@ -19,64 +19,56 @@ class PurePursuitNode(Node):
             [-37.224998, -58.524998],   # third corner
             [-37.224998, -51.524998],   # fourth corner
             [-38.224998, -51.224998],   # fourth corner
-            [-60.224998, -51.224998]    # loop to first    
+            [-60.224998, -51.224998]    # fourth to first    
         ]) 
         # https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.splprep.html examples
         self.tck, _ = splprep([targets[:,0], targets[:,1]], s=0)
         
         self.drive_pub = self.create_publisher(AckermannDriveStamped, '/drive', 10)
         self.scan_sub = self.create_subscription(LaserScan, '/scan', self.scan_callback, 10)
-        self.lookahead = 1.0
-       # self.current_position = np.array([-51.224998, -51.224998]) # initialized to starting
-        self.current_position = np.array([0, 0])
+        
+        self.L = 0.3    # wheelbase
+        self.kv = 0.5   # lookahead distance gain
+        self.current_heading = 0
+        self.speed = 1.0
+        self.current_position = np.array([-51.224998, -51.224998]) # initialized to starting point
         self.theta = 0
-        self.wheelbase = 1.0
         
     def scan_callback(self, msg):    
-        u = np.linspace(0,1, num = 100)
+        u = np.linspace(0,1, num=100)
         spline_points = np.array(splev(u, self.tck)).T
-        lookahead_point = self.find_lookahead(spline_points, self.current_position, self.lookahead)
-        steering_angle, speed = self.control(lookahead_point, self.current_position, self.theta)
         
-        self.update_position(steering_angle, speed)
+        lookahead_point = self.find_lookahead_point(spline_points)
+        
+        steering_angle = self.calculate_steering_angle(lookahead_point)
         
         drive_msg = AckermannDriveStamped()
-        drive_msg.drive.speed = speed
+        drive_msg.drive.speed = self.speed
         drive_msg.drive.steering_angle = steering_angle
+        
         self.drive_pub.publish(drive_msg)
         
-        self.get_logger().info(f"Position: {self.current_position}, Theta: {self.theta}, Steering: {steering_angle}")
+    def find_lookahead_point(self, points):
+        dl = self.kv * self.speed   # eq in book chapter
         
-    def calculate_distance(self, point1, point2):
-        dx = point1[0] - point2[0]
-        dy = point1[1] - point2[1]
-        return np.sqrt(dx**2 + dy**2)
-    
-    def find_lookahead(self, points, position, lookahead):
         for point in points:
-            distance = self.calculate_distance(point, position)
-            if distance >= lookahead:
+            distance = np.linalg.norm(point - self.current_position)  # sqrt(x^2 + y^2)
+            if distance >= dl:
                 return point
         return points[-1]
     
-    def control(self, lookahead_point, current_position, theta):
-        dx = lookahead_point[0] - current_position[0]
-        dy = lookahead_point[1] - current_position[1]
-        d = np.sqrt(dx**2 + dy**2)
-        alpha = np.arctan2(dy,dx)
+    def calculate_steering_angle(self, lookahead_point):
+        dx = lookahead_point[0] - self.current_position[0]
+        dy = lookahead_point[1] - self.current_position[1]
         
-        heading_error = alpha - theta
+        lookahead_angle = np.arctan2(dy,dx)
+        alpha = lookahead_angle - self.current_heading
         
-        steering_angle = np.arctan2(2.0 * self.wheelbase * np.sin(heading_error), d)
-        speed = 1.0
-        return steering_angle, speed
-    
-    def update_position(self, steering_angle, speed):
-        dt = 0.1  
-        self.theta += steering_angle * speed * dt
-        self.current_position[0] += speed * np.cos(self.theta) * dt
-        self.current_position[1] += speed * np.sin(self.theta) * dt
+        dl = np.linalg.norm([dx,dy])
+        steering_angle = np.arctan2(2 * self.L * np.sin(alpha), dl)
         
+        return steering_angle
+         
 
 def main(args=None):
     rclpy.init(args=args)  
