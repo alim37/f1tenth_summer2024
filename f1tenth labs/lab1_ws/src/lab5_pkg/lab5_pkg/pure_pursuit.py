cfc5934c9beb650e77586166ed190e0899f8b38c
Malik Ali <158620884+alim37@users.noreply.github.com>
2024-07-01 11:17:48 -0400

    Update pure_pursuit.py

--- f1tenth labs/lab1_ws/src/lab5_pkg/lab5_pkg/pure_pursuit.py
+++ f1tenth labs/lab1_ws/src/lab5_pkg/lab5_pkg/pure_pursuit.py
@@ -39,36 +39,57 @@ class PurePursuitNode(Node):
             [8.9921, 8.3980],
             [7.7689, 8.4908],
             [7.0000, 8.5759],
-            [6.0000, 8.5759],
-            [5.0000, 8.5759],
-            [4.0000, 8.5759],
-            [3.0000, 8.5759],
-            [2.0000, 8.5759],
-            [1.0000, 8.5759],
-            [0.0000, 8.5759],
-            [-1.0000, 8.5759],
-            [-2.0000, 8.5759],
-            [-3.0000, 8.5759],
-            [-4.0000, 8.5759],
-            [-5.0000, 8.5759],
-            [-6.0000, 8.5759],
-            [-7.0000, 8.5759],
-            [-8.0000, 8.5759],
-            [-9.0000, 8.5759],
-            [-10.0000, 8.5759],
-            [-11.0000, 8.5759],
-            [-12.0000, 8.5759],
-            [-12.4353, 8.5759],
-            [-12.9307, 8.3914],
-            [-13.3309, 7.8879],
-            [-13.6016, 6.5028],
-            [-13.7000, 6.0000],
-            [-13.7000, 5.0000],
-            [-13.7000, 4.0000],
-            [-13.7000, 3.0000],
-            [-13.7000, 2.0000],
-            [-13.7000, 1.0000],
-            [-13.5205, 0.3887],
+            
+           # [6.0000, 8.5759],
+            #[5.0000, 8.5759],
+           # [4.0000, 8.5759],
+           # [3.0000, 8.5759],
+           # [2.0000, 8.5759],
+          #  [1.0000, 8.5759],
+            #[0.0000, 8.5759],
+           # [-1.0000, 8.5759],
+           # [-2.0000, 8.5759],
+           # [-3.0000, 8.5759],
+          #  [-4.0000, 8.5759],
+          #  [-5.0000, 8.5759],
+         #   [-6.0000, 8.5759],
+          #  [-7.0000, 8.5759],
+         #   [-8.0000, 8.5759],
+           # [-9.0000, 8.5759],
+           # [-10.0000, 8.5759],
+           # [-11.0000, 8.5759],
+          #  [-12.0000, 8.5759],
+            #[-12.4353, 8.5759],
+            [6.0000, 9.1],
+            [5.0000, 9.1],
+            [4.0000, 9.1],
+            [3.0000, 9.1],
+            [2.0000, 9.1],
+            [1.0000,9.1],
+            [0.0000, 9.1],
+            [-1.0000, 9.1],
+            [-2.0000, 9.1],
+            [-3.0000, 9.1],
+            [-4.0000, 9.1],
+            [-5.0000, 9.1],
+            [-6.0000, 9.1],
+            [-7.0000, 9.1],
+            [-8.0000, 9.1],
+            [-9.0000, 9.1],
+            [-10.0000, 9.1],
+            [-11.0000, 9.1],
+            [-12.0000, 9.1],
+            [-13.0, 9.1],
+            #[-12.9307, 8.3914],
+            #[-13.3309, 7.8879],
+            #[-13.6016, 6.5028],
+            [-14, 6.0000],
+            [-14, 5.0000],
+            [-14, 4.0000],
+            [-14, 3.0000],
+            [-14, 2.0000],
+            [-14, 1.0000],
+            [-14, 0.3887],
             [-13.0705, -0.0388],
             [-12.0000, 0],  
             [-11.0000, 0],
@@ -86,27 +107,23 @@ class PurePursuitNode(Node):
                 
         self.current_target_idx = 0
 
-        self.lookahead_distance = 1.0
-        self.wheelbase = 0.3
+        self.lookahead_distance = 2.0
         self.max_steering_angle = 0.5
+        
+        self.wheelbase = 0.3
 
         self.drive_pub =self.create_publisher(AckermannDriveStamped, '/drive', 10)
         self.odom_sub = self.create_subscription(Odometry, '/ego_racecar/odom', self.odom_callback, 10)
 
         self.create_timer(0.1, self.pure_pursuit)
-        self.current_pose = None
     
     def odom_callback(self, msg):
         self.current_pose = msg.pose.pose
-        #self.get_logger().info('test')
         self.get_logger().info(f'Received odom: x={self.current_pose.position.x:.2f}, y={self.current_pose.position.y:.2f}')
 
     def pure_pursuit(self):
-        if self.current_pose is None:
-            return
-        
         if self.current_target_idx >= len(self.targets):
-            self.get_logger().info('Reached final target')
+            self.get_logger().info('End of lap')
             return
         
         current_target = self.targets[self.current_target_idx]
@@ -119,18 +136,42 @@ class PurePursuitNode(Node):
             self.current_target_idx += 1
             return
         
-        y = dy 
-        steering_angle = 2 * abs(y) / (self.lookahead_distance**2)
-        steering_angle = min(steering_angle, self.max_steering_angle)
+        _, _, yaw = self.euler_from_quaternion(self.current_pose.orientation)
         
-        if y < 0:
-            steering_angle = -steering_angle
+        goal_x_veh = dx * math.cos(yaw) + dy*math.sin(yaw)
+        goal_y_veh = -dx * math.sin(yaw) + dy * math.cos(yaw)
+        
+        y = dy 
+        #steering_angle = 2 * abs(y) / (self.lookahead_distance**2)
+        #steering_angle = min(steering_angle, self.max_steering_angle)
+        
+        steering_angle = math.atan2(2*self.wheelbase * goal_y_veh, distance**2)
+        #steering_angle = max(-self.max_steering_angle, min(self.max_steering_angle, steering_angle))
 
         drive_msg = AckermannDriveStamped()
         drive_msg.drive.speed = 1.0
         drive_msg.drive.steering_angle = steering_angle
 
         self.drive_pub.publish(drive_msg)
+        
+    def euler_from_quaternion(self, quaternion):
+        x = quaternion.x
+        y = quaternion.y
+        z = quaternion.z
+        w = quaternion.w
+
+        sinr_cosp = 2 * (w * x + y * z)
+        cosr_cosp = 1 - 2 * (x * x + y * y)
+        roll = math.atan2(sinr_cosp, cosr_cosp)
+
+        sinp = 2 * (w * y - z * x)
+        pitch = math.asin(sinp)
+
+        siny_cosp = 2 * (w * z + x * y)
+        cosy_cosp = 1 - 2 * (y * y + z * z)
+        yaw = math.atan2(siny_cosp, cosy_cosp)
+
+        return roll, pitch, yaw
 
 
 def main(args=None):
